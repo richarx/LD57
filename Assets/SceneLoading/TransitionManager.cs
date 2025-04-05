7a6cc9dc9ab2521f99a202f67071edc377832bc3
richarx <theorichard12@gmail.com>
2025-04-06 01:41:57 +0200

    Destroy computer transition after use

--- Assets/SceneLoading/TransitionManager.cs
+++ Assets/SceneLoading/TransitionManager.cs
@@ -56,6 +56,7 @@ namespace SceneLoading
                     yield return null;
                     duration -= Time.deltaTime;
                 }
+                Destroy(currentSpriteRenderer.gameObject);
             }
         }
 
