f39e571596943586fcb9fb314a6a358c244a294a
richarx <theorichard12@gmail.com>
2025-04-06 01:20:55 +0200

    Added transition computer

--- Assets/SceneLoading/TransitionManager.cs
+++ Assets/SceneLoading/TransitionManager.cs
@@ -9,8 +9,10 @@ namespace SceneLoading
         [SerializeField] private Image blackScreen;
         [SerializeField] private GameObject oliveTransition;
         [SerializeField] private GameObject teaTransition;
+        [SerializeField] private GameObject computerTransition;
 
         private Transform currentTransition;
+        private SpriteRenderer currentSpriteRenderer;
         
         public IEnumerator PlayTransition(string sceneName, bool transitionIN)
         {
@@ -20,13 +22,46 @@ namespace SceneLoading
                 yield return TeaTransition(transitionIN);
             else if (sceneName.Equals("Martini"))
                 yield return OliveTransition(transitionIN);
+            else if (sceneName.Equals("Computer"))
+                yield return ComputerTransition(transitionIN);
             else
                 yield return Tools.Fade(blackScreen, 0.7f, transitionIN);
         }
-        
-        private IEnumerator TeaTransition(bool transitionIN)
+
+        private IEnumerator ComputerTransition(bool transitionIn)
         {
-            if (transitionIN)
+            if (transitionIn)
+            {
+                Vector2 startPosition = new Vector2(0.0f, -5.1f);
+                currentSpriteRenderer = Instantiate(computerTransition, startPosition, Quaternion.identity, transform).GetComponent<SpriteRenderer>();
+
+                currentSpriteRenderer.size = new Vector2(10.0f, 0.0f);
+                
+                float duration = 0.7f;
+                float delta = 5.625f / duration;
+                while (duration >= 0.0f)
+                {
+                    currentSpriteRenderer.size = new Vector2(10.0f, currentSpriteRenderer.size.y + delta * Time.deltaTime);
+                    yield return null;
+                    duration -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                float duration = 0.7f;
+                float delta = 5.625f / duration;
+                while (duration >= 0.0f)
+                {
+                    currentSpriteRenderer.size = new Vector2(10.0f, currentSpriteRenderer.size.y - delta * Time.deltaTime);
+                    yield return null;
+                    duration -= Time.deltaTime;
+                }
+            }
+        }
+
+        private IEnumerator TeaTransition(bool transitionIn)
+        {
+            if (transitionIn)
             {
                 Vector2 startPosition = new Vector2(0.0f, 13.0f);
                 currentTransition = Instantiate(teaTransition, startPosition, Quaternion.identity, transform).transform;
@@ -54,9 +89,9 @@ namespace SceneLoading
             }
         }
 
-        private IEnumerator OliveTransition(bool transitionIN)
+        private IEnumerator OliveTransition(bool transitionIn)
         {
-            if (transitionIN)
+            if (transitionIn)
             {
                 Vector2 startPosition = new Vector2(-25.5f, 0.0f);
                 currentTransition = Instantiate(oliveTransition, startPosition, Quaternion.identity, transform).transform;
