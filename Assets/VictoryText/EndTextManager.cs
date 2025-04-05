f91abce59729a3fe2663fef1c0c32e472d26419d
richarx <theorichard12@gmail.com>
2025-04-05 18:14:21 +0200

    Added success and fail face

--- Assets/VictoryText/EndTextManager.cs
+++ Assets/VictoryText/EndTextManager.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace VictoryText
 {
     public class EndTextManager : MonoBehaviour
     {
         [SerializeField] private GameObject textPrefab;
+        [SerializeField] private GameObject faceSuccessPrefab;
+        [SerializeField] private GameObject faceFailurePrefab;
 
         public static EndTextManager instance;
 
@@ -15,14 +19,25 @@ namespace VictoryText
             instance = this;
         }
 
-        public void SpawnText(string text, float delay, float displayDuration)
+        private void Update()
         {
-            StartCoroutine(SpawnTextCoroutine(text, delay, displayDuration));
+            if (Input.GetMouseButtonDown(0))
+                SpawnText("Yay !", 0.0f, 1.5f, true);
+            if (Input.GetMouseButtonDown(1))
+                SpawnText("Nay !", 0.0f, 1.5f, false);
         }
 
-        private IEnumerator SpawnTextCoroutine(string text, float delay, float displayDuration)
+        public void SpawnText(string text, float delay, float displayDuration, bool success)
+        {
+            StartCoroutine(SpawnTextCoroutine(text, delay, displayDuration, success));
+        }
+
+        private IEnumerator SpawnTextCoroutine(string text, float delay, float displayDuration, bool success)
         {
             yield return new WaitForSeconds(delay);
+
+            GameObject face = Instantiate(success ? faceSuccessPrefab : faceFailurePrefab, Vector2.zero + (Random.insideUnitCircle.normalized * 2.5f), Quaternion.identity);
+            face.transform.rotation = Vector2.right.AddRandomAngleToDirection(-25.0f, 25.0f).ToRotation();
             
             GameObject textObject = Instantiate(textPrefab, transform.position, Quaternion.identity, transform);
 
@@ -32,6 +47,7 @@ namespace VictoryText
             textObject.transform.rotation = Vector2.right.AddRandomAngleToDirection(-25.0f, 25.0f).ToRotation();
 
             Destroy(textObject, displayDuration);
+            Destroy(face, displayDuration);
         }
     }
 }
