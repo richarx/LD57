29913bab4dc456bf5d3c37b8d5fcffc27aab023d
richarx <theorichard12@gmail.com>
2025-04-06 13:00:46 +0200

    Mouth transition

--- Assets/SceneLoading/TransitionManager.cs
+++ Assets/SceneLoading/TransitionManager.cs
@@ -6,10 +6,12 @@ namespace SceneLoading
 {
     public class TransitionManager : MonoBehaviour
     {
+        [SerializeField] private float transitionDuration;
         [SerializeField] private Image blackScreen;
         [SerializeField] private GameObject oliveTransition;
         [SerializeField] private GameObject teaTransition;
         [SerializeField] private GameObject computerTransition;
+        [SerializeField] private GameObject mouthTransition;
 
         private Transform currentTransition;
         private SpriteRenderer currentSpriteRenderer;
@@ -24,8 +26,49 @@ namespace SceneLoading
                 yield return OliveTransition(transitionIN);
             else if (sceneName.Equals("Computer"))
                 yield return ComputerTransition(transitionIN);
+            else if (sceneName.Equals("Glotte"))
+                yield return MouthTransition(transitionIN);
             else
                 yield return Tools.Fade(blackScreen, 0.7f, transitionIN);
+        }
+
+        private IEnumerator MouthTransition(bool transitionIn)
+        {
+            if (transitionIn)
+            {
+                currentTransition = Instantiate(mouthTransition, Vector2.zero, Quaternion.identity, transform).transform;
+
+                Transform mouthUp = currentTransition.GetChild(0);
+                Transform mouthDown = currentTransition.GetChild(1);
+                
+                float duration = transitionDuration;
+                float deltaUp = 6.5f / duration;
+                float deltaDown = 8.5f / duration;
+                while (duration >= 0.0f)
+                {
+                    mouthUp.position = new Vector3(0.0f, mouthUp.position.y - deltaUp * Time.deltaTime, 0.0f);
+                    mouthDown.position = new Vector3(0.0f, mouthDown.position.y + deltaDown * Time.deltaTime, 0.0f);
+                    yield return null;
+                    duration -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                Transform mouthUp = currentTransition.GetChild(0);
+                Transform mouthDown = currentTransition.GetChild(1);
+                
+                float duration = transitionDuration;
+                float deltaUp = 6.5f / duration;
+                float deltaDown = 8.5f / duration;
+                while (duration >= 0.0f)
+                {
+                    mouthUp.position = new Vector3(0.0f, mouthUp.position.y + deltaUp * Time.deltaTime, 0.0f);
+                    mouthDown.position = new Vector3(0.0f, mouthDown.position.y - deltaDown * Time.deltaTime, 0.0f);
+                    yield return null;
+                    duration -= Time.deltaTime;
+                }
+                Destroy(currentTransition.gameObject);
+            }
         }
 
         private IEnumerator ComputerTransition(bool transitionIn)
@@ -37,7 +80,7 @@ namespace SceneLoading
 
                 currentSpriteRenderer.size = new Vector2(10.0f, 0.0f);
                 
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 5.625f / duration;
                 while (duration >= 0.0f)
                 {
@@ -48,7 +91,7 @@ namespace SceneLoading
             }
             else
             {
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 5.625f / duration;
                 while (duration >= 0.0f)
                 {
@@ -67,7 +110,7 @@ namespace SceneLoading
                 Vector2 startPosition = new Vector2(0.0f, 13.0f);
                 currentTransition = Instantiate(teaTransition, startPosition, Quaternion.identity, transform).transform;
 
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 13.0f / duration;
                 while (duration >= 0.0f)
                 {
@@ -78,7 +121,7 @@ namespace SceneLoading
             }
             else
             {
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 13.0f / duration;
                 while (duration >= 0.0f)
                 {
@@ -97,7 +140,7 @@ namespace SceneLoading
                 Vector2 startPosition = new Vector2(-25.5f, 0.0f);
                 currentTransition = Instantiate(oliveTransition, startPosition, Quaternion.identity, transform).transform;
 
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 25.5f / duration;
                 while (duration >= 0.0f)
                 {
@@ -108,7 +151,7 @@ namespace SceneLoading
             }
             else
             {
-                float duration = 0.7f;
+                float duration = transitionDuration;
                 float delta = 25.5f / duration;
                 while (duration >= 0.0f)
                 {
